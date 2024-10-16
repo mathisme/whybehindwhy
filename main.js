1aa38bc1aa3fdb9222d36542bd6b8ca01b884e18
Anne Losch <annedl1211@gmail.com>
2024-10-16 06:49:14 -0500

    started working on it

--- main.js
+++ main.js
@@ -1,44 +1,53 @@
 console.log("testing");
 
-/*
-my idea for the model (not very good but I think a db model isn't needed)
-is two arrays.  The first array contains the bottom level (the last thing 
-added to the wish/goal or the wish/goal iself) and a nested array where
-each sublist is the items in the wish/goal
-there is also a boolean firstUse variable which is used by the controller
-to determine how to set the view
-*/
-
 const model = {
-    firstUse : true,
-    init : function(){
-        // I dont think an init is needed as the controller will do this
-        // unless the controller just saves the strings and the model parses
-
-    }
+  firstUse : true,
+  listUpdated : [],
+  init : function(){
+    for (let i=0; i < model.items.length; i++){model.listUpdated[i]=0;}
+  }
 
 };
 
+const view = {
+  listCreated : false,
+  wish_input : document.getElementById("wish_input"),
+  wish_submit : document.getElementById("wish_submit"),
+  wish_input_form : document.getElementById("form"),
+  wishlist_div : document.getElementById("wishlist"),
+  workbook_div : document.getElementById("workbook")
+};
 
-const view = {};
 const controller = {
     // will also need to add functions for event listeners
+  listView : function(){
+    // need to check the view if list was created
+    // if so, just need to unhide
+    // remember will also need to update anything new
+    // so need something in the model to say list updated
+    // if not, need to update the listview ui for the first time
     
-    init : function(){
-        if (navigator.storage && navigator.storage.persist) {
-            navigator.storage.persist().then((persistent) => {
-              if (persistent) {
-                console.log("Storage will not be cleared except by explicit user action");
-              } else {
-                console.log("Storage may be cleared by the UA under storage pressure.");
-              }
-            });
-          }
-        // need to check if the local storage has wish bottom level
-        // or whatever.  If it does save all data to the model
-        // and set model firstUse equal to false 
-        // need to finish the init fuction         
+  },  
+  init : function(){
+    if (navigator.storage && navigator.storage.persist) {
+      navigator.storage.persist().then((persistent) => {
+      if (persistent) {
+        console.log("Storage will not be cleared except by explicit user action");
+      } else {
+        console.log("Storage may be cleared by the UA under storage pressure.");
+      }
+      });
     }
+
+    if (localStorage.getItem("wbwLowestLevel"))
+      {
+        model.firstUse = false;
+        model.items = JSON.parse(localStorage.getItem("wbwItems"));
+        model.archived = JSON.parse(localStorage.getItem("wbwarchives"));
+        model.init();
+      }
+        // need to finish the init fuction         
+  }
 };
 
 controller.init();
